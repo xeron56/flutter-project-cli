587f733d21f43b398550814daf53040b52f58d77
MD. SHAHIDUL ISLAM <shahidul1425@cseku.ac.bd>
2026-06-29 17:18:45 +0600

    refactor: remove unused assistant UI screens and update router configuration

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_app_template/features/assistant_ui/assistant_ui.dart';
-import 'package:flutter_bloc_app_template/features/assistant_ui/primary_screens.dart';
 import 'package:flutter_bloc_app_template/features/launch/view/launch_screen.dart';
 import 'package:go_router/go_router.dart';
 
 abstract final class AppRoutes {
   static const home = '/';
-  static const chat = '/chat';
-  static const goals = '/goals';
-  static const email = '/email';
-  static const automations = '/automations';
-  static const more = '/more';
-  static const family = '/family';
-  static const finance = '/finance';
-  static const calendar = '/calendar';
-  static const wellness = '/wellness';
-  static const vault = '/vault';
   static const launchDetail = '/launch/:flightNumber';
 
   static String launchFor(int flightNumber) => '/launch/$flightNumber';
@@ -29,57 +17,7 @@ final appRouter = GoRouter(
     GoRoute(
       path: AppRoutes.home,
       pageBuilder: (_, state) =>
-          _fadeTransition(state, const AssistantHomeScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.chat,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const AssistantChatScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.goals,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const AssistantGoalsScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.email,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const AssistantDocsEmailScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.automations,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const AssistantAutomationScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.more,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const AssistantMoreScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.family,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const FamilyRelationshipsScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.finance,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const FinanceInsightsScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.calendar,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const CalendarPlannerScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.wellness,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const HealthWellnessScreen()),
-    ),
-    GoRoute(
-      path: AppRoutes.vault,
-      pageBuilder: (_, state) =>
-          _fadeTransition(state, const PrivacyVaultScreen()),
+          _fadeTransition(state, const LaunchScreen(flightNumber: 1)),
     ),
     GoRoute(
       path: AppRoutes.launchDetail,
